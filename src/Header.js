d4fe76b1ee3a0e9d636e0e51d3e885dbd5c53dcb
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-13 19:20:34 +0530

    header left & middle part completed

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReorderIcon from "@mui/icons-material/Reorder";
+import SearchIcon from "@mui/icons-material/Search";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { IconButton } from "@material-ui/core";
 import "./Header.css";
 
 const Header = () => {
@@ -7,7 +10,26 @@ const Header = () => {
     <>
       <div className="header">
         <div className="header__left">
-          <ReorderIcon></ReorderIcon>
+          <IconButton>
+            <ReorderIcon></ReorderIcon>
+          </IconButton>
+          <img
+            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+            alt="logo"
+            width="100"
+            height="40"
+          />
+        </div>
+        <div className="header__middle">
+          <div className="search_mail">
+            <IconButton>
+              <SearchIcon></SearchIcon>
+            </IconButton>
+            <input type="text" placeholder="Search mail" />
+            <IconButton>
+              <ExpandMoreIcon></ExpandMoreIcon>
+            </IconButton>
+          </div>
         </div>
       </div>
     </>
