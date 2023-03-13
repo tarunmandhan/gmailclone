85d161cdcff7d4bb55a69f55af4be7745b7b4055
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-13 13:24:18 +0530

    init 2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <Header />
+      </div>
+    </>
+  );
+};
+
+export default App;
