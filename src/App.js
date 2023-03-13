e052f6625e2d9b8bb5842f09b60ba0884872dbcb
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-13 23:56:31 +0530

    add comp. Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const App = () => {
   return (
     <>
       <div className="App">
         <Header />
+        <Sidebar />
       </div>
     </>
   );
