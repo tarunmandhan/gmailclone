4cc6a5428492abff942e3ec7dcb6189b5c6ed37b
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-13 23:02:17 +0530

    Header right part & Avatar

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReorderIcon from "@mui/icons-material/Reorder";
 import SearchIcon from "@mui/icons-material/Search";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Avatar } from "@material-ui/core";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import AppsIcon from "@mui/icons-material/Apps";
 import "./Header.css";
 
 const Header = () => {
@@ -30,6 +33,24 @@ const Header = () => {
               <ExpandMoreIcon></ExpandMoreIcon>
             </IconButton>
           </div>
+        </div>
+        <div className="header__right">
+          <IconButton>
+            <HelpOutlineOutlinedIcon></HelpOutlineOutlinedIcon>
+          </IconButton>
+          <IconButton>
+            <SettingsOutlinedIcon></SettingsOutlinedIcon>
+          </IconButton>
+          <IconButton>
+            <AppsIcon></AppsIcon>
+          </IconButton>
+
+          <Avatar
+            alt="Tarun"
+            src="https://media.licdn.com/dms/image/C5603AQE4MyDDjgEsZw/profile-displayphoto-shrink_800_800/0/1649276317754?e=2147483647&v=beta&t=BEfnpHDQAKaQOg3sQrTTnyJmV7IygQs49K4ZUe3O2fY"
+          >
+            T
+          </Avatar>
         </div>
       </div>
     </>
