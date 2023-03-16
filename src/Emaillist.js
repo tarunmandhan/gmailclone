24dfd61d9a257c469ac5bd967a5fe85a272e8ede
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-17 00:44:41 +0530

    imp Comp.

--- src/Emaillist.js
+++ src/Emaillist.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Emaillist.css";
 import EmailListSetting from "./EmailListSetting";
+import Emailtype from "./Emailtype";
 
 const Emaillist = () => {
   return (
     <div className="emaillist">
       <EmailListSetting />
+      <Emailtype />
     </div>
   );
 };
