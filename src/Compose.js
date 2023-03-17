97be812e4face8a60a2f7c237e983315ddbf1b6a
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-18 01:36:46 +0530

    Compose header part complete

---
+++ src/Compose.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Compose.css";
+import RemoveIcon from "@mui/icons-material/Remove";
+import OpenInFullIcon from "@mui/icons-material/OpenInFull";
+import CloseIcon from "@mui/icons-material/Close";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import FormatColorTextIcon from "@mui/icons-material/FormatColorText";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import LinkIcon from "@mui/icons-material/Link";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import PhotoIcon from "@mui/icons-material/Photo";
+import PhonelinkLockIcon from "@mui/icons-material/PhonelinkLock";
+import CreateIcon from "@mui/icons-material/Create";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const Compose = () => {
+  return (
+    <div className="compose">
+      <div className="compose__header">
+        <div className="compose__header__left">
+          <span>New Message</span>
+        </div>
+        <div className="compose__header__right">
+          <RemoveIcon />
+          <OpenInFullIcon />
+          <CloseIcon />
+        </div>
+      </div>
+      <div className="compose__body"></div>
+      <div className="compose__footer"></div>
+    </div>
+  );
+};
+
+export default Compose;
