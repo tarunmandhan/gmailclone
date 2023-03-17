bd20575937ba60e7506784869677a3814802af44
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-18 00:33:18 +0530

    using props  passing from Emaillist

--- src/Emailbody.js
+++ src/Emailbody.js
@@ -4,24 +4,24 @@ import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import LabelOutlinedIcon from "@mui/icons-material/LabelOutlined";
 
-const Emailbody = () => {
+const Emailbody = ({ name, subject, message, time }) => {
   return (
     <div className="emailbody">
       <div className="emailbody__left">
         <CheckBoxOutlineBlankIcon />
         <StarBorderIcon />
         <LabelOutlinedIcon />
-        <h4>Tarun Mandhan</h4>
+        <h4>{name}</h4>
       </div>
       <div className="emailbody__middle">
         <div className="emailbody__middle__msg">
           <p>
-            <b>Subject</b> This is massage body
+            <b>{subject}</b> {message}
           </p>
         </div>
       </div>
       <div className="emailbody__right">
-        <p>7:25 PM</p>
+        <p>{time}</p>
       </div>
     </div>
   );
