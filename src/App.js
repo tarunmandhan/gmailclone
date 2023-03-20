47b1fc98a70e659a04c74f52eac359d11f1e8f6c
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-21 04:03:54 +0530

    useSelector hooks (Redux)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Sidebar from "./Sidebar";
 import "./App.css";
 import Emaillist from "./Emaillist";
 import Compose from "./Compose";
+import { useSelector } from "react-redux";
+import { selectSendMessageIsOpen } from "./features/mailSlice";
 
 const App = () => {
-  const isMessageOpen = true;
+  const isMessageOpen = useSelector(selectSendMessageIsOpen);
+  console.log(isMessageOpen);
   return (
     <>
       <div className="App">
